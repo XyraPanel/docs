ebb3413d3f2e1a028e000aeee564b9b55cd9bf83
26bz <26bz@proton.me>
2025-11-14 21:37:42 -0500

    feat: enhance articles

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,23 @@
 export default defineAppConfig({
+  header: {
+    // Title to display if no logo
+    title: 'XyraPanel',
+    // Logo configuration
+    logo: {
+      alt: '',
+      // Light mode
+      light: '',
+      // Dark mode
+      dark: ''
+    },
+  },
+  socials: {
+    discord: 'https://discord.com/invite/NXdctQnMjs',
+  },
+  github: {
+    url: 'https://github.com/XyraPanel/docs',
+    branch: 'main',
+  },
   ui: {
     colors: {
       primary: 'blue',
