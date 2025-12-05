e6b15ade3459e0ee4c15c101bf8f57f9f3d695b2
26bz <26bz@proton.me>
2025-12-05 01:07:02 -0500

    feat: add TOC

--- app.config.ts
+++ app.config.ts
@@ -18,6 +18,24 @@ export default defineAppConfig({
     url: 'https://github.com/XyraPanel/docs',
     branch: 'main',
   },
+  toc: {
+    title: 'On this page',
+    bottom: {
+      title: 'Community',
+      links: [{
+        icon: 'ic:baseline-discord',
+        label: 'Join our Discord',
+        to: 'https://discord.com/invite/NXdctQnMjs',
+        target: '_blank'
+      },
+      {
+        icon: 'material-icon-theme:github-sponsors',
+        label: 'Become a Sponsor',
+        to: 'https://ko-fi.com/26bz',
+        target: '_blank'
+      }]
+    }
+  },
   ui: {
     colors: {
       primary: 'blue',
