3c47739c48d16f13b0647bb2270e66bdf70ddd54
26bz <26bz@proton.me>
2025-12-09 21:07:59 -0500

    chore: update attribution

--- components/AppFooterLeft.vue
+++ components/AppFooterLeft.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="text-sm text-muted">
-    Copyright &copy; {{ new Date().getFullYear() }} XyraPanel | Created by 26bz & <ULink target="_blank"
+    Copyright &copy; {{ new Date().getFullYear() }} XyraPanel | Created by <ULink target="_blank"
+      to="https://26bz.com/">26bz</ULink> & <ULink target="_blank"
       to="https://github.com/XyraPanel/panel/graphs/contributors">Contributors</ULink> |
     <ULink target="_blank" to="https://github.com/XyraPanel/panel/blob/main/LICENSE">MIT Licensed</ULink>
   </div>
