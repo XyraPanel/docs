0167e5ffd3a14bf481ceb0da3a67d1b0e24580ff
26bz <26bz@proton.me>
2026-01-01 15:19:51 -0500

    chore: bump copyright year

--- components/AppFooterLeft.vue
+++ components/AppFooterLeft.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-sm text-muted">
-    Copyright &copy; {{ new Date().getFullYear() }} XyraPanel | Created by <ULink target="_blank"
+    Copyright &copy; 2025-{{ new Date().getFullYear() }} XyraPanel | Created by <ULink target="_blank"
       to="https://26bz.com/">26bz</ULink> & <ULink target="_blank"
       to="https://github.com/XyraPanel/panel/graphs/contributors">Contributors</ULink> |
     <ULink target="_blank" to="https://github.com/XyraPanel/panel/blob/main/LICENSE">MIT Licensed</ULink>
